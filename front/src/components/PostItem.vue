f807c4c881e8377c999a69ab2e4aa44c22810fb4
lim kyoung tae <limkt0925@gmail.com>
2024-05-21 00:16:18 +0900

    new community & like update

--- front/src/components/PostItem.vue
+++ front/src/components/PostItem.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="wrap">
     <div class="header">
-      <p>안뇽</p>
-      <p>user1</p>
+      <p>{{ post.title }}</p>
+      <p>{{ post.user.nickname }}</p>
     </div>
-    <p>content</p>
+    <div class="main">
+      <p>{{ post.content.slice(0, 20) }}...</p>
+      <p>&hearts; {{ likeLength }}</p>
+    </div>
     <hr>
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  
+import { ref, computed, defineProps } from 'vue'
 
+const props = defineProps({
+  post: {
+    type: Object,
+    required: true
+  }
+})
+
+const likeLength = computed(() => {
+  return props.post.like_users.length
+})
 </script>
 
 <style scoped>
@@ -20,6 +32,11 @@
   padding: 0 3%;
 }
 .header {
+  display: flex;
+  justify-content: space-between;
+  font-weight: 700;
+}
+.main {
   display: flex;
   justify-content: space-between;
 }
